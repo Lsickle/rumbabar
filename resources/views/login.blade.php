d7523dc528dc3d1c574d71094ce7d0dfcfbd049c
luis de la hoz <davidoy69@hotmail.com>
2020-11-05 11:00:45 -0500

    se ajustan los estilos de las vistas para login y registro queda pendiente los colores de los degradados

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -5,10 +5,11 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-  <title>Laravel</title>
+  <title>Login - Rumbabar</title>
 
   <!-- Fonts -->
   <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Revalia&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="css/app.css">
 
   <!-- Styles -->
@@ -16,53 +17,47 @@
 </head>
 
 <body>
-	<div class="content bg-gradient-secondary p-3 min-vh-100">
+	<div class="content p-3 min-vh-90" id="mainContent" style="background-color: F2F0F9;">
 		<div class="container">
-			@if (Route::has('login'))
-			<nav class="navbar navbar-expand-lg navbar-light">
-				<a class="navbar-brand" href="#"><i loading="lazy" width="36" height="36"
-						class="d-inline-block align-top fab fa-rockrms fa-lg"></i>umbaBar</a>
-				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-					aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-					<span class="navbar-toggler-icon"></span>
-				</button>
-			
-				<ul class="nav justify-content-end collapse navbar-collapse" id="navbarSupportedContent">
+			<nav class="navbar navbar-light">
+				<a class="navbar-brand" href="#"><i loading="lazy" width="36" height="36" class="d-inline-block align-top fab fa-rockrms fa-lg"></i>umbaBar</a>
+				<ul class="nav justify-content-end" id="navbarSupportedContent">
 					@auth
 					<li class="nav-item">
-						<a class="nav-link btn btn-outline-dark m-2" href="{{ route('home') }}"><span
-								class="sr-only">(current)</span>Home</a>
+						<a class="nav-link btn btn-outline-dark m-2" href="{{ route('home') }}">Home</a>
 					</li>
 					@else
-					{{-- <li class="nav-item">
-						<a class="nav-link btn btn-outline-dark m-2 active" href="{{ route('login') }}">Login</a>
-					</li> --}}
+						@if (Route::has('register'))
+						<li class="nav-item">
+							<a class="nav-link btn btn-outline-dark m-2" href="{{ route('register') }}">Registro</a>
+						</li>
+						@endif
 					@endauth
-					@if (Route::has('register'))
-					<li class="nav-item">
-						<a class="nav-link btn btn-outline-dark m-2" href="{{ route('register') }}">Registro</a>
-					</li>
-					@endif
 				</ul>
 			</nav>
-			@endif
-			<div class="jumbotron py-5 h-100" style="background-image: url('img/bar-background.jpg');background-repeat: no-repeat;
+			<div class="jumbotron py-5 h-90" style="background-image: url('img/bar-background-offcolor.webp');background-repeat: no-repeat;
   background-size: cover;background-position:center center;">
 				<div class="row">
-					<div class="col-lg-5">
-						<div class="card bg-transparent" style="width: 18rem;background-image: linear-gradient(rgba(60, 1, 128, 0.6), rgba(0, 26, 130, 0.6), rgba(23, 201, 0, 0.6));">
+					<div class="col-xl-5">
+						<div class="card bg-transparent min-vh-90" style="background-image: linear-gradient(rgba(60, 1, 128, 0.6), rgba(0, 26, 130, 0.6), rgba(23, 201, 0, 0.6));">
 							<img class="card-img-top" src="img/espacioParaLogo.png" alt="Card image cap">
 							<div class="card-body">
-								{{-- pendiente instalacion de fuente "revalia" --}}
-								<h3 class="card-title text-left">Login</h3>
+								<h2 style="font-family: 'Revalia', cursive; 
+								color:white;
+    background: -webkit-linear-gradient(#030095, #4465dd);
+    background: -o-linear-gradient(#030095, #4465dd);
+    background: -moz-linear-gradient(#030095, #4465dd);
+    background: linear-gradient(#030095, #4465dd);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;" class="card-title text-left">Login</h2>
 								<form>
-									<div class="card border-0 rounded p-2">
+									<div class="card border-0 rounded p-3">
 										<div class="form-group">
-											<input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
-												placeholder="ingrese su Usuario">
+											<input name="username" type="email" class="form-control" id="exampleInputUser1" aria-describedby="userHelp"
+												placeholder="Ingrese su Usuario">
 										</div>
 										<div class="form-group">
-											<input type="password" class="form-control" id="exampleInputPassword1" placeholder="ingrese su Contraseña">
+											<input name="contraseña" type="password" class="form-control" id="exampleInputPassword1" placeholder="Ingrese su Contraseña">
 										</div>
 										<button type="submit" class="btn btn-primary">Ingresar</button>
 									</div>
