85d81b9dbd1c96fac3073b19ddb41cf797aeda4a
luis de la hoz <davidoy69@hotmail.com>
2020-11-05 11:03:15 -0500

    se incluye la fuente revalia en la vista de registro

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -9,6 +9,7 @@
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Revalia&display=swap" rel="stylesheet"> 
     <link rel="stylesheet" href="css/app.css">
 
     <!-- Styles -->
