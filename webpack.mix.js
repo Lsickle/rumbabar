0ffedf2b30e8f6631d05692b6876f97411c6cd37
Luis De la hoz <luisdelahoz0@gmail.com>
2021-03-23 20:20:40 -0500

    ignored public js and css

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,7 +14,7 @@ const mix = require('laravel-mix');
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css')
     .copy('node_modules/@fortawesome/fontawesome-free/webfonts', 'public/webfonts')
-	.js('resources/js/scriptspersonalizados.js', 'public/js/scriptspersonalizados.js');
+	.copy('resources/js/scriptspersonalizados.js', 'public/js/scriptspersonalizados.js');
 
 if (mix.inProduction()) {
     mix.version();
