53473390963985a2c4d34bd058e8e0945794c1d9
Luis De la hoz <luisdelahoz0@gmail.com>
2021-03-23 20:16:43 -0500

    resolve compilin assets

--- resources/views/Producto/edit.blade.php
+++ resources/views/Producto/edit.blade.php
@@ -121,8 +121,8 @@ Editar Producto
         console.log('hola');
     });
 </script>
-<script type="text/javascript">
-	@include('scriptspersonalizados')
-</script>
+@push('scripts')
+<script src="{{ asset('js/components/scriptspersonalizados.js')}}"></script>
+@endpush
 <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js" integrity="sha512-VEd+nq25CkR676O+pLBnDW09R7VQX9Mdiij052gVCp5yVH3jGtH70Ho/UUv4mJDsEdTvqRCFZg0NKGiojGnUCw==" crossorigin="anonymous"></script>
 @endsection
--- resources/views/layouts/adminApp.blade.php
+++ resources/views/layouts/adminApp.blade.php
@@ -2,34 +2,34 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    <link rel="shortcut icon" href="/img/favicon.png">
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta name="csrf-token" content="{{ csrf_token() }}">
+	<link rel="shortcut icon" href="/img/favicon.png">
 
-    <title>@yield('pagename')</title>
+	<title>@yield('pagename')</title>
 
-    <!-- Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Revalia&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
-    
-    <!-- Styles -->
-    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+	<!-- Fonts -->
+	<link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css2?family=Revalia&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
 
-    @yield('styles')
+	<!-- Styles -->
+	<link rel="stylesheet" href="{{asset('css/app.css')}}">
+
+	@yield('styles')
 
 </head>
 
 <body>
-    <div id="app">
-        <main class="content min-vh-90 bg-light">
-            @yield('header')
-            @yield('container')
-        </main>
-    </div>
+	<div id="app">
+		<main class="content min-vh-90 bg-light">
+			@yield('header')
+			@yield('container')
+		</main>
+	</div>
 	<script src="{{asset('js/app.js')}}"></script>
-    @yield('scripts')
+	@stack('scripts')
 </body>
 
 </html>
