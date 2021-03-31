4fff438e247654a1e8f3a5e0f8952133f0f99d32
Luis De la hoz <luisdelahoz0@gmail.com>
2021-03-30 21:48:22 -0500

    commented return

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -29,12 +29,12 @@ class HomeController extends Controller
 		$ultimaventa = Venta::all()->last();
         $totalventaslastmonth = Venta::whereDate('updated_at', '>', Carbon::now()->subDays(30))->get('VentaTotal')->sum('VentaTotal');
         $numeroventaslastmonth = Venta::whereDate('updated_at', '>', Carbon::now()->subDays(30))->get()->count();
-        
+
 		$ultimacompra = Compra::all()->last();
         $totalcompraslastmonth = Compra::whereDate('updated_at', '>', Carbon::now()->subDays(30))->get('CompraTotal')->sum('CompraTotal');
         $numerocompraslastmonth = Compra::whereDate('updated_at', '>', Carbon::now()->subDays(30))->get()->count();
 
-        return $numerocompraslastmonth;
+        // return $numerocompraslastmonth;
         return view('home', compact(['ultimaventa', 'ultimacompra', 'totalventaslastmonth']));
     }
 }
